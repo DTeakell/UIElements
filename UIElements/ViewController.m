2d79efd81997d162c6bcecb59dd962564498de11
Dillon Teakell <teakelld@icloud.com>
2025-05-16 11:19:50 -0400

    Added a TextField to display a greeting to the user

--- UIElements/ViewController.m
+++ UIElements/ViewController.m
@@ -9,6 +9,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UITextField *textField;
 @property (nonatomic, strong) UIButton *button;
 
 @end
@@ -25,9 +26,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
  
-    // 1. Create label
-    self.label = [[UILabel alloc]initWithFrame:CGRectMake(50, 100, 150, 50)];
-    self.label.text = @"Hello. Objective-C!";
+    // Create label
+    self.label = [[UILabel alloc]initWithFrame:CGRectMake(50, 50, 50, 50)];
+    self.label.text = @"Please enter your name";
     self.label.textAlignment = NSTextAlignmentCenter;
     self.label.textColor = [UIColor blackColor];
     
@@ -36,7 +37,16 @@
     [self.view addSubview: self.label];
     
     
-    // 2. Create button
+    // Create text field
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(50, 50, 50, 50)];
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField.placeholder = @"Name";
+    self.textField.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview: self.textField];
+    
+    
+    // Create button
     self.button = [UIButton buttonWithType: UIButtonTypeSystem];
     self.button.frame = CGRectMake(50, 100, 150, 50);
     [self.button setTitle: @"Confirm" forState: UIControlStateNormal];
@@ -48,23 +58,37 @@
     [self.view addSubview: self.button];
     
     
-    // 3. Add constraints
+    // Add constraints
     [NSLayoutConstraint activateConstraints: @[
         // Center label and place it near the top
         [self.label.centerXAnchor constraintEqualToAnchor: self.view.centerXAnchor],
         [self.label.topAnchor constraintEqualToAnchor: self.view.safeAreaLayoutGuide.topAnchor constant: 150],
         
+        [self.textField.centerXAnchor constraintEqualToAnchor: self.view.centerXAnchor],
+        [self.textField.topAnchor constraintEqualToAnchor: self.label.bottomAnchor constant: 20],
+        
         // Center button horizontally and place it under the button
         [self.button.centerXAnchor constraintEqualToAnchor: self.view.centerXAnchor],
         [self.button.topAnchor constraintEqualToAnchor:
-            self.label.bottomAnchor constant: 50]
+            self.textField.bottomAnchor constant: 20]
     ]];
 }
 
+// Function that changes the label to welcome the user
+- (void) greetUser {
+    NSString *name = self.textField.text;
+    
+    if (name.length > 0) {
+        self.label.text = [NSString stringWithFormat: @"Hello, %@!", name];
+    } else {
+        self.label.text = @"Hello there!";
+    }
+}
+
+// Function for when the button is tapped
 - (void) buttonTapped {
     NSLog(@"Button Tapped");
-    self.label.textColor = [UIColor orangeColor];
-    self.label.text = @"The label is now orange!";
+    [self greetUser];
 }
 
 
